4686861f7db9afd3948d3561a654c6b12bd577f1
Edoardo Ascenzi <edoardo.ascenzi97@gmail.com>
2024-06-09 19:30:57 +0200

    setted up all the controls of the music palyer available till now

--- React/app/src/components/MusicPlayer.jsx
+++ React/app/src/components/MusicPlayer.jsx
@@ -42,10 +42,15 @@ function MusicPlayer(props) {
 
 
 function Controls(props) {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const audioRef = useRef(null);
-  const [progress, setProgress] = useState(0);
+  //init as true to start the song after selected it from the result list
+  const [isPlaying, setIsPlaying] = useState(true); 
+  const audioRef = useRef(null); //reference to the audio
+  const [progress, setProgress] = useState(0); //state of the progress bar
+  const progressBarRef = useRef(null);
+  const [repeat, setRepeat] = useState("")
+
   const handlePlayPause = () => {
+    //set the audio reference state according the play/pause button pressiong
     if (isPlaying) {
       audioRef.current.pause();
     } else {
@@ -54,44 +59,115 @@ function Controls(props) {
     setIsPlaying(!isPlaying);
   };
 
+  const skipSeconds = () => {
+    let skipInterval = 10
+    if (audioRef.current.duration > (30*60)) skipInterval = 15
+    audioRef.current.currentTime = audioRef.current.currentTime + skipInterval
+    updateProgress()
+  }
+
+  const rewindSeconds = () => {
+    let skipInterval = 10
+    if (audioRef.current.duration > (30*60)) skipInterval = 15
+    audioRef.current.currentTime = audioRef.current.currentTime - skipInterval
+    updateProgress()
+  }
+
+  const repeatSingleSong = () => {
+    if(repeat === "single") setRepeat("")
+    else  setRepeat("single")
+  }
+
+  const handleEnded = () => {
+    if (repeat === ""){
+      audioRef.current.currentTime=0
+      audioRef.current.pause();
+      setIsPlaying(false)
+    }
+    else if(repeat === "single"){
+      audioRef.current.currentTime=0
+      audioRef.current.play();
+      setIsPlaying(true)
+
+    }
+  }
+
+  const handleProgressClick = (e) => {
+    const progressBar = progressBarRef.current;
+    const rect = progressBar.getBoundingClientRect();
+    const offsetX = e.clientX - rect.left;
+    const clickPosition = offsetX / rect.width;
+    const newTime = clickPosition * audioRef.current.duration;
+    audioRef.current.currentTime = newTime;
+    setProgress(clickPosition * 100);
+  };
+
   const updateProgress = () => {
+    // update the progress bar state
     const currentTime = audioRef.current.currentTime;
     const duration = audioRef.current.duration;
     setProgress((currentTime / duration) * 100);
   };
 
   useEffect(() => {
+    // the effect with dependecies is need to call this effect every time the song chages
     if (isPlaying) {
       audioRef.current.play();
     }
-  }, [props.song.id]);
+  }, [props.song.id]); 
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (audioRef.current) {
-        updateProgress();
-      }
-    }, 250);
+      // if the audio is playing calls the updateProgress every 250ms
+      if (audioRef.current) updateProgress();
+        }, 250);
     return () => clearInterval(interval);
   }, []);
+
+  const formatTime = (time) => {
+    const hours = Math.floor(time / (60*60))
+    const minutes = Math.floor((time-(hours*60*60)) / 60 );
+    const seconds = Math.floor((time-(hours*60*60)) % 60);
+    return hours > 0 ? `${hours}:${minutes}:${seconds < 10 ? '0' : ''}${seconds}` : `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+  };
   
 
   return <>
   <Container>
   <Stack gap={3}>
-  <audio ref={audioRef} src={props.song.music} />
-  <ProgressBar variant='secondary' animated  now={progress}/>
+
+  <audio ref={audioRef} src={props.song.music} onEnded={handleEnded} />
+
+    <Row className="align-items-center">
+      <Col xs="auto"  className="d-flex align-items-center">
+      <span className='badge bg-secondary'>{audioRef.current ? formatTime(audioRef.current.currentTime) : formatTime(0)}</span>
+      </Col>
+      <Col ref={progressBarRef} onClick={handleProgressClick} style={{cursor : 'pointer'}}>
+        <ProgressBar variant='secondary' animated  now={progress} />
+      </Col>
+      <Col xs="auto" className="d-flex align-items-center">
+      <span className='badge bg-secondary'>{audioRef.current ? formatTime(audioRef.current.duration) : formatTime(0)}</span>
+      </Col>
+    </Row>
+
+
     <Row><Col>
-    <Button variant=''><i className="bi bi-shuffle"></i></Button>{" "}
-    <Button variant='secondary'><i className="bi bi-skip-start-fill"></i></Button>{" "}
-    <Button variant='secondary'><i className="bi bi-arrow-counterclockwise"></i></Button>{" "}
+    <Button variant='' disabled><i className="bi bi-shuffle"></i></Button>{" "}
+    <Button variant='secondary' disabled><i className="bi bi-skip-start-fill"></i></Button>{" "}
+    <Button variant='secondary' onClick={rewindSeconds}><i className="bi bi-arrow-counterclockwise"></i></Button>{" "}
+    
     <Button variant='secondary' onClick={handlePlayPause} >
       {isPlaying ? <i className="bi bi-pause-fill"></i> : <i className="bi bi-play-fill"></i>}
     </Button> {" "}
-    <Button variant='secondary'><i className="bi bi-arrow-clockwise"></i></Button>{" "}
-    <Button variant='secondary'><i className="bi bi-skip-end-fill"></i></Button>{" "}
-    <Button variant='secondary'><i className="bi bi-repeat"></i></Button>{" "}
-    <Button variant='secondary'><i className="bi bi-repeat-1"></i></Button>
+
+    <Button variant='secondary' onClick={skipSeconds}><i className="bi bi-arrow-clockwise"></i></Button>{" "}
+    <Button variant='secondary' disabled ><i className="bi bi-skip-end-fill"></i></Button>{" "}
+    <Button variant='secondary' disabled><i className="bi bi-repeat"></i></Button>{" "}
+
+    <Button variant='secondary' onClick={repeatSingleSong}>
+      {repeat === "single" ? <i style={{color:"blue"}} className="bi bi-repeat-1"></i> : <i className="bi bi-repeat-1"></i>}
+    </Button>
+    
     </Col></Row>
     <Row><Col>
     <Button variant="secondary">Add to Playlist</Button> {" "}
--- React/app/src/components/modules.mjs
+++ React/app/src/components/modules.mjs
@@ -10,7 +10,7 @@ export function Song(id, title, music, description = "", image = "./gandalf.gif"
 export const FAKE_SONGS = [
     new Song(1, "Set 1","./Jamie xx Boiler Room London x Young Turks DJ Set.mp3", "Channel 1", "./hqdefault.webp"),
     new Song(2, "Set 2" ,"./What Happens In One Minute.mp3" ),
-    new Song(3, "Set 3","./Jamie xx Boiler Room London x Young Turks DJ Set.mp3", "Ahahahaaha", "./mqdefault_6s.webp")
+    new Song(3, "Set 3","./Four Tet & Ben UFO - Mix for Hessle Audio  Rinse FM.mp3", "Ahahahaaha", "./mqdefault_6s.webp")
 ]
 
 
